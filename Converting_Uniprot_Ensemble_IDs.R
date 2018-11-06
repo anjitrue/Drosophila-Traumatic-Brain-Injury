4366f8d74a97424d1e2d94ed63fefc435cf324a2
anjitrue <etrujillo2@wisc.edu>
2018-11-06 16:11:25 -0600

    Confirm the id's provided in the table.

--- Converting_Uniprot_Ensemble_IDs.R
+++ Converting_Uniprot_Ensemble_IDs.R
@@ -16,12 +16,13 @@ library(dplyr)
 proteinGroups_Ensembl_reducedtoLFQ <- read_excel("E:/Projects/Proteomics/DiversityOutcross/txt_proteinEnsembl/proteinGroups_Ensmbl_reducedtoLFAQ.xlsx") 
 proteinGroups_Uniprot_reducedtoLFQ <- read_excel("E:/Projects/Proteomics/DiversityOutcross/txt_Uniprot/proteinGroups_Uniprot_reducedtoLFQ.xlsx") 
 
+# vectors containing the protein.ids
 Ensembl_protein_id <- proteinGroups_Ensembl_reducedtoLFQ$`Protein IDs`
 Uniprot_protein_id <- proteinGroups_Uniprot_reducedtoLFQ$`Protein IDs`
 
-
-Ensembl_join <- data.frame(proteinGroups_Ensembl_reducedtoLFQ[,c(1,2)])
-rownames(Ensembl_join) <- Ensembl_join$`Protein.IDs`
+# Subset dataframe to only the protein IDs
+Uniprot_join <- data.frame(proteinGroups_Uniprot_reducedtoLFQ[,c(1,2)])
+rownames(Uniprot_join) <- Uniprot_join$`Protein.IDs`
 #protein_id <- Uniprot_test$Protein.IDs
 
 #--------------------------------------------- BiomaRt Code -------------------------------------------
@@ -33,29 +34,55 @@ ensembl <- useDataset("mmusculus_gene_ensembl", mart = ensembl) #select dataset 
 filters = listFilters(ensembl) #filter options
 attributes <- listAttributes(ensembl) #values interested in retreiving from query
 
-searchFilters(mart = ensembl, pattern = "mmus")
-searchAttributes(mart = ensembl, pattern = "mmus")
+searchFilters(mart = ensembl, pattern = "uniprot")
+searchAttributes(mart = ensembl, pattern = "ensembl")
 
-#filterType("with_uniprotswissprot", ensembl)
-
-transcript_mouse_ID_Ensembl <- getBM(attributes = c("ensembl_transcript_id", "uniprotsptrembl", "uniprotswissprot"),
+# Create matching table for Uniprot protein.ids
+transcript_mouse_ID_Ensembl <- getBM(attributes = c("ensembl_transcript_id","uniprotsptrembl", "uniprotswissprot"),
                              values = Ensemble_protein_id,
-                             #filters = "gene_name",
+                             filters = "ensembl_transcript_id",
                              mart = ensembl)
 
+transcript_mouse_ID_Uniprot <- getBM(attributes = c("ensembl_transcript_id", "uniprotsptrembl", "uniprotswissprot"),
+                                     values = Uniprot_protein_id,
+                                     #filters = "uniprotswissprot", # important filter to obtain only UniprotIDs in list
+                                     mart = ensembl)
+
+# Matching to existing list
 transcript_mouse_ID_Ensembl <- data.frame(transcript_mouse_ID_Ensembl)
 colnames(transcript_mouse_ID_Ensembl)[which(names(transcript_mouse_ID_Ensembl) == "ensembl_transcript_id")] <- "Protein.IDs"
 
 
-Ensembl_join = right_join(transcript_mouse_ID_Ensembl, Ensembl_join, by = "Protein.IDs")
+Uniprot_join = right_join(transcript_mouse_ID_Ensembl, Uniprot_join, by = "Protein.IDs")
 
-write.table(transcript_mouse_ID_Ensembl, "E:/Projects/Proteomics/DiversityOutcross/txt_proteinEnsembl/Rmodified_EnsembleID_matchingTable.txt", sep="\t")
+write.table(transcript_mouse_ID_Uniprot, "E:/Projects/Proteomics/DiversityOutcross/txt_Uniprot/Rmodified_Uniprot_matchingTable.txt", sep="\t")
 
+i = 1
+match_id <- NULL
+for(i in 1:nrow(transcript_mouse_ID_Uniprot)){
+  if(!(transcript_mouse_ID_Uniprot[i,2] == ""| is.na(transcript_mouse_ID_Uniprot[i,2]) )){
+   if (length(grep(transcript_mouse_ID_Uniprot[i,2], Uniprot_protein_id) > 0)){ 
+     x = grep(transcript_mouse_ID_Uniprot[i,2], Uniprot_protein_id)
+     } else(x = NA)
+  } else(x = NA)
+  if(!(transcript_mouse_ID_Uniprot[i,3] == "" | is.na(transcript_mouse_ID_Uniprot[i,2]))){
+    if (length(grep(transcript_mouse_ID_Uniprot[i,3], Uniprot_protein_id)) > 0){ 
+      y = grep(transcript_mouse_ID_Uniprot[i,3], Uniprot_protein_id) } else(y = NA)
+  } else(y = NA)
+  match_id <- append(match_id, paste(x,y))
+}
+match_id <- gsub(" ", "", match_id)
+match_id <- gsub("NA", "", match_id)
+match_id <- sub(";", "", match_id)
+
+table(match_id)
 
 #--------------------------------------------- AnnotationHub Code -------------------------------------------                             
 
 hub <- query(AnnotationHub(), c('ensembl', 'gtf', 'mus musculus'))
 hub <- hub[grep('GRCm38', hub$title)]
 anno_hub <- hub[["AH60127"]]
+
+#--------------------------------------------- Data Upload --------------------------------------------
 
 #union <- Ensembl_test[which(rownames(filtered) %in% Ensembl_test$Protein.IDs),]
