f76f7f23eeff1b26051ef163963d2c264879ce6a
anjitrue <etrujillo2@wisc.edu>
2018-12-05 10:43:55 -0600

    heatmaps

--- Loading_LFQdatasets.R
+++ Loading_LFQdatasets.R
@@ -3,7 +3,8 @@ source("https://bioconductor.org/biocLite.R")
 biocLite("pcaMethods")
 
 devtools::install_github("xia-lab/MetaboAnalystR", build_vignettes=TRUE)
-
+BiocManager::install("ComplexHeatmap", version = "3.8")
+BiocManager::install("topGO", version = "3.8")
 library(readr)
 library(plyr)
 library(dplyr)
@@ -14,6 +15,13 @@ library(devtools)
 library(e1071)
 library(dplyr)
 library(Mfuzz)
+library(circlize)
+library(ComplexHeatmap)
+library(RColorBrewer)
+library(viridis)
+library(topGO)
+install.packages('GOplot')
+library(GOplot)
 
 ##################################### Load Data ####################################################
 
@@ -98,24 +106,57 @@ remove.features.10percentcuttoff <- function (x) {
   return(filtered)
 }
 
-fuzzyprep_imputation_included <- function(z)
-{
+filter.std.plotting <- function (eset, min.std,visu=TRUE){
+  #index <- logical(dim(exprs(eset))[1])
+  
+  tmp <- logical(dim(exprs(eset))[1])
+  if (is.numeric(min.std))
+  { 
+    data <- exprs(eset)
+    for (i in 1:length(tmp))
+    {
+      tmp[i]   <- sd(data[i,],na.rm=TRUE)
+      #index[i]  <- ( tmp[i] > min.std)
+      
+    }
+    index <- tmp > min.std
+    index[is.na(index)] <- TRUE
+    cat(paste(sum(!index),"genes excluded.\n"))
+  }
+  
+  if (visu)
+  {
+    plot(sort(tmp),xlab="Ordered Hemo Proteins",ylab="Standard Deviation")
+  }
+  eset[index,]
+}
+
+fuzzyprep_imputation_included <- function(z){
   exprValues <- new("ExpressionSet", exprs = as.matrix(z))
   # exclude proteins that have more than 50% of measurements missing
   exprValues.r <- filter.NA(exprValues, thres = 0.50)
+  # Fuzzy c-means does not allow for missing values, replace missing values by median values
   exprValues.f = fill.NA(exprValues.r, mode="median")
+  #
   tmp = filter.std(exprValues.f, min.std = 0.1)
+  # Clustering is performed in Eculidian space, standaridize abundance values to have a mean value of zero
+  # Ensures that proteins with similar changes in abundance are close in Euclidean space
   exprValues.s = standardise(tmp)
   return(exprValues.s)
 }
 
-fuzzyprep_usepreviousImputation <- function(z)
-{
+fuzzyprep_usepreviousImputation <- function(z){
   exprValues <- new("ExpressionSet", exprs = as.matrix(z))
-  # exclude proteins that have more than 50% of measurements missing
-  exprValues.r <- filter.NA(exprValues, thres = 0.50)
-  tmp = filter.std(exprValues.r, min.std = 0.1)
-  exprValues.s = standardise(tmp)
+  tmp = filter.std.plotting(exprValues, min.std = 0.1, visu = TRUE)
+  exprValues.s = standardise(exprValues)
+  return(exprValues.s)
+}
+
+fuzzyprep_usepreviousImputation_foldchange <- function(z)
+{
+  exprValues.s <- new("ExpressionSet", exprs = as.matrix(z))
+  tmp = filter.std.plotting(exprValues.s, min.std = 0.1)
+  #exprValues.s = standardise(exprValues)
   return(exprValues.s)
 }
 
@@ -143,81 +184,209 @@ filtered_dros_hemo_10percent <- remove.features.10percentcuttoff(proteinGroups_d
 #[1] "Remaining Features: 9181"
 #[1] "Features Removed: 0"
 
-even_index <- seq(2,8,2)
-odd_index <- seq(1,8,2)
+
 
 # set up for baysian pca imputation
 hemo_50percent <- as.matrix(filtered_dros_hemo_50percent[,c(4:11)])
 hemo_50percent_log2 <- log2(hemo_50percent)
 rownames(hemo_50percent_log2) <- filtered_dros_hemo_50percent$id
 
-pc_hemolog2 <- pca(hemo_50percent_log2, nPcs = 3, method = "bpca")
+pc_hemolog2 <- pca(t(hemo_50percent_log2), nPcs = 3, method = "bpca") #pca method
 imputed_hemo <- completeObs(pc_hemolog2)
 
+palette(c("mediumorchid2","mediumturquoise","olivedrab3", "darkgoldenrod1", 
+          "hotpink3", "red2", "steelblue2", "sienna2","slategray4", 
+          "deepskyblue", "orangered", "midnightblue"))
+
+plot(scores(pc_hemolog2), pch = 16,  main = "PCAScores Drosophila Hemolympn")
+text(scores(pc_hemolog2)[,1], scores(pc_hemolog2)[,2], labels = colnames(hemo_50percent_log2))
+
+# split data up between the controls and the TBI samples
+even_index <- seq(2,8,2) #TBI samples
+odd_index <- seq(1,8,2) #controls
 imputed_hemo_even <- imputed_hemo[,even_index]
 imputed_hemo_odd <- imputed_hemo[,odd_index]
+imputed_hemo_reorder <- imputed_hemo[,c(odd_index,even_index)]
+
+#Fold change
+imputed_hemo_fold <- imputed_hemo_reorder[,5:8]-imputed_hemo_reorder[,1:4]
 
 
-#subset for other modes of imputation
+# subset for other types of imputation
 z_hemo <- proteinGroups_dros_hemo[,4:11]
 z_hemo <- log2(z_hemo)
 rownames(z_hemo) <- proteinGroups_dros_hemo$id
 Z_hemo_even <- z_hemo[,even_index]
 z_hemo_odd <- z_hemo[,odd_index]
 
+# subset features with fold change greater than...
+important_features_hemo_foldchange <- imputed_hemo_fold[rowSums(abs(imputed_hemo_fold)>1.2)>0,]
+
 # Hemo mFuzz Soft Clustering----------------------------------------------------
-z = imputed_hemo_odd #change according to data input
-
-
-exprValues.s_imput <- fuzzyprep_imputation_included(Z_hemo_even)
-exprValues.s_bayesian <- fuzzyprep_usepreviousImputation(imputed_hemo_even)
-
-m1 <- mestimate(exprValues.s_imput)
-error <- NA
-
-for(i in 2:15){
-  c1 <- mfuzz(exprValues.s_imput, c=i, m=m1)
-  error <- rbind(error, c(i,c1$withinerror))
-}
-plot(error[,1], error[,2])
-
-
+z = important_features_hemo_foldchange # change accordingly to use in mfuzzy setup funtions
+exprValues.s_imput <- fuzzyprep_imputation_included(Z)
+exprValues.s_bayesian <- fuzzyprep_usepreviousImputation(z)
+exprValues.s_bayesian_foldchange <- fuzzyprep_usepreviousImputation_foldchange(z)
 #exprValues.s.nominstd = standardise(exprValues.r) #use data that is non std
 
-m1 <- mestimate(exprValues.s)
+
 
 error <- NA
 
+y = exprValues.s_bayesian_foldchange#change according to version of data to perform fuzzy clustering
+m1 <- mestimate(y) 
+
+
 for(i in 2:15){
-  c1 <- mfuzz(exprValues.s, c=i, m=m1)
-  error <- rbind(error, c(i,c1$withinerror))
+  c2 <- mfuzz(y, c=i, m=m1)
+  error <- rbind(error, c(i,c2$withinerror))
 }
+
 plot(error[,1], error[,2])
 
-c1 <- mfuzz(exprValues.s, c=6, m=m1)
+set.seed(123)
+c1 <- mfuzz(y, c=6, m=m1)
+c2 <- mfuzz(y, c=4, m=m1)
+#pdf(file = "20181022_Phosphosites_SoftClusters.pdf")
+#par(mar=c(3,3,1,1))
+mfuzz.plot2(y, cl=c1, mfrow = c(3,2), time.labels = c("1hr","4hr", "8hr", "24hr","1hr","4hr", "8hr", "24hr"), 
+            ylab = "Protein Changes", xlab = "Hemo Time Points",  col.lab="black", x11=F)
+mfuzz.plot2(y, cl=c1, mfrow = c(3,2), min.mem = 0.70, time.labels = c("1hr","4hr", "8hr", "24hr","1hr","4hr", "8hr", "24hr"), 
+            ylab = "Protein Changes", xlab = "Hemo Time Points",  col.lab="black", x11=F)
 
-pdf(file = "20181022_Phosphosites_SoftClusters.pdf")
-par(mar=c(3,3,1,1))
-mfuzz.plot2(exprValues.s, cl=c1, mfrow = c(3,2), col.lab="black", x11=F)
+mfuzz.plot2(y, cl=c2, mfrow = c(4,2), time.labels = c("1hr","4hr", "8hr", "24hr"), 
+            ylab = "Protein Changes", xlab = "Hemo Time Points",  col.lab="black", x11=F)
+mfuzz.plot2(y, cl=c2, mfrow = c(4,2), time.labels = c("1hr","4hr", "8hr", "24hr"), 
+            min.mem = 0.50, ylim.set = c(-2,2), ylab = "Protein Changes", xlab = "Hemo Time Points",  col.lab="black", x11=F)
+
+### Fuzzy membership filter
+#compare two feature clusters (fold change*)
+
+table(rowSums(c1$membership > .70) >0)
+
+#dev.off()
+
+a_bayesian <- acore(exprValues.s_bayesian_foldchange, c2, min.acore=0.0)
+
+cluster1 <- data.frame(a_bayesian[[1]])
+cluster2 <- data.frame(a_bayesian[[2]])
+cluster3 <- data.frame(a_bayesian[[3]])
+cluster4 <- data.frame(a_bayesian[[4]])
+#For Phospho Data
+cluster5 <- data.frame(a_bayesian[[5]])
+cluster6 <- data.frame(a_bayesian[[6]])
+
+
+
+names.FromCluster_2 <- cluster2$NAME
+cluster3_imputed_hemo_reorder <- imputed_hemo_reorder[rownames(imputed_hemo_reorder) %in% names.FromCluster, ]
+imputed_hemo_cluster3_fold <- as.matrix(cluster3_imputed_hemo_reorder[,5:8]-cluster3_imputed_hemo_reorder[,1:4])
+
+rownames(proteinGroups_dros_hemo) <- proteinGroups_dros_hemo$id
+cluster2_hemo_proteins <-  proteinGroups_dros_hemo[rownames(proteinGroups_dros_hemo) %in% names.FromCluster,]
+genes_cluster2 <- cluster2_hemo_proteins$Gene.names
+write.csv(cluster2_hemo_proteins, "cluster2_hemo_proteins.csv")
+
+scaleRYG <- colorRampPalette(c("red","black","darkgreen"), space = "rgb")(31)
+dis <- dist(imputed_hemo_cluster2_fold,method = "euclidean")
+
+distance = dist(imputed_hemo_cluster2_fold, method = "euclidean")
+row.cluster = hclust(imputed_hemo_cluster2_fold, method = "aver")
+
+
+
+breaksList = c(-10,seq(-2, 2, length.out = 28),10)
+pheatmap(imputed_hemo_cluster2_fold, color = colorRampPalette(rev(brewer.pal(n = 5, name = "YlOrRd")))(length(breaksList)), # Defines the vector of colors for the legend (it has to be of the same lenght of breaksList)
+         breaks = breaksList) 
+
+pheatmap(
+  mat               = imputed_hemo_reorder[rowSums(c1$membership >0.7)>0,],
+  color             = scaleRYG,
+  border_color      = NA,
+  scale = "row",
+  cluster_cols = FALSE,
+  cluster_rows = TRUE,
+  clustering_distance_rows = "euclidean",
+  show_colnames     = TRUE,
+  show_rownames     = FALSE,
+  #cellwidth = 40,
+  #cellheight = .25,
+  #annotation_col    = mat_col,
+  #annotation_colors = mat_colors,
+  drop_levels       = TRUE,
+  fontsize          = 10,
+  main              = "Default Heatmap"
+)
+
+table(rowSums(abs(imputed_hemo_fold)>0.5)>0)
+p <- pheatmap(
+  mat               = imputed_hemo_fold[rowSums(abs(imputed_hemo_fold)>1.2)>0,], #[rowSums(c2$membership >0.5)>0,],
+  color             = scaleRYG,
+  border_color      = NA,
+  #scale = "row",
+  cluster_cols = FALSE,
+  cluster_rows = TRUE,
+  breaks = breaksList,
+  clustering_distance_rows = "euclidean",
+  show_colnames     = TRUE,
+  show_rownames     = FALSE,
+  #cellwidth = 40,
+  #cellheight = .25,
+  #annotation_col    = mat_col,
+  #annotation_colors = mat_colors,
+  drop_levels       = TRUE,
+  fontsize          = 10,
+  main              = "Default Heatmap"
+)
 dev.off()
 
-a <- acore(exprValues.s, c1, min.acore=0.0)
+important_features_foldchange_compress <- important_features_hemo_foldchange
+important_features_foldchange_compress[important_features_foldchange_compress < -2] = -2.1
+important_features_foldchange_compress[important_features_foldchange_compress > 2] = 2.1
 
-cluster1 <- data.frame(a[[1]])
-cluster2 <- data.frame(a[[2]])
-cluster3 <- data.frame(a[[3]])
-cluster4 <- data.frame(a[[4]])
-#For Phospho Data
-cluster5 <- data.frame(a[[5]])
-cluster6 <- data.frame(a[[6]])
 
-write.csv(cluster1, "cluster1Info.csv")
-write.csv(cluster2, "cluster2Info.csv")
-write.csv(cluster3, "cluster3Info.csv")
-write.csv(cluster4, "cluster4Info.csv")
-#For Phospho Data
-write.csv(cluster5, "cluster5Info.csv")
-write.csv(cluster6, "cluster6Info.csv")
+pheatmap(
+  mat               = important_features_foldchange_compress, #[rowSums(c2$membership >0.5)>0,],
+  color             = scaleRYG,
+  border_color      = NA,
+  #scale = "row",
+  cluster_cols = FALSE,
+  cluster_rows = TRUE,
+  #breaks = breaksList,
+  clustering_distance_rows = "euclidean",
+  show_colnames     = TRUE,
+  show_rownames     = FALSE,
+  #cellwidth = 40,
+  #cellheight = .25,
+  #annotation_col    = mat_col,
+  #annotation_colors = mat_colors,
+  drop_levels       = TRUE,
+  fontsize          = 10,
+  main              = "Default Heatmap"
+)
+dev.off()
+fold_hclust <- hclust(dist(important_features_foldchange_compress, method = "euclidean"))
+fold_hclust <- agnes(dist(imputed_hemo_fold[rowSums(abs(imputed_hemo_fold)>1.2)>0,], method = "euclidean"))
+
+clusters <- cutree(fold_hclust, k = 4) #k = 20)
+
+plot(fold_hclust, label= FALSE)
+
+#rect.hclust(fold_hclust, k = 20, border = "red")
+rect.hclust(fold_hclust, k=3, border = "red")
+
+library(cluster)
+cluster2 <- kmeans(t(as.matrix(imputed_hemo_fold[rowSums(abs(imputed_hemo_fold)>1.2)>0,])),10)
+
+clusplot(important_features_foldchange_compress, clusters, lines = 0)
+clusplot(imputed_hemo_fold[rowSums(abs(imputed_hemo_fold)>1.2)>0,], cluster2, lines = 0)
+
+write.csv(cluster1, "HemoOrdered_bayesian_cluster1Info.csv")
+write.csv(cluster2, "HemoOrdered_bayesian_cluster2Info.csv")
+write.csv(cluster3, "HemoOrdered_bayesian_cluster3Info.csv")
+write.csv(cluster4, "HemoOrdered_bayesian_cluster4Info.csv")
+write.csv(cluster5, "HemoOrdered_bayesian_cluster5Info.csv")
+write.csv(cluster6, "HemoOrdered_bayesian_cluster6Info.csv")
 
 
 
